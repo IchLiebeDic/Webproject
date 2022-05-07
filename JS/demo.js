bd2e0771d3bb93c80c00d605cc0c2c62180f1da8
AnhJob2011 <anh218120@gmail.com>
2022-05-08 02:45:13 +0700

    Release Webproject

---
+++ JS/demo.js
@@ -0,0 +1,7 @@
+function addFavor(){
+    // alert("Đã thêm vào Tủ Sách");
+    window.confirm('Đã thêm vào Tủ Sách ');
+}
+function changecolor() {
+    document.getElementById("main").style.backgroundColor = "red";
+  }
